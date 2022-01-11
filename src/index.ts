7cc498f1b6966e47a17925e25f07bdb493f7a753
Ryota Mizumaki <statham.programming@gmail.com>
2022-01-11 17:49:31 +0900

    export mockServer for node

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { setupWorker } from "msw";
+import { setupServer } from "msw/node";
 import { getRequestHandlers } from "./getRequestHandlers";
 import { MockDefinition, MockProxySetting } from "./type";
 
@@ -25,4 +26,10 @@ export const setupYockMock = (option: YockMockOption) => {
   const worker = setupWorker(...getRequestHandlers(option));
 
   return worker;
+};
+
+// TODO: Extract this into another module (like 'yock-mock/node')
+export const setupYockMockServer = (option: YockMockOption) => {
+  const server = setupServer(...getRequestHandlers(option));
+  return server;
 };
