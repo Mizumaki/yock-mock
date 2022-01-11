a9a9c687cadfacd3b7a2ef3f1f9d9052e7f22c61
Ryota Mizumaki <statham.programming@gmail.com>
2022-01-11 20:02:33 +0900

    temporary remove setupYockMockServer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { setupWorker } from "msw";
-import { setupServer } from "msw/node";
+// import { setupServer } from "msw/node";
 import { getRequestHandlers } from "./getRequestHandlers";
 import { MockDefinition, MockProxySetting } from "./type";
 
@@ -29,7 +29,7 @@ export const setupYockMock = (option: YockMockOption) => {
 };
 
 // TODO: Extract this into another module (like 'yock-mock/node')
-export const setupYockMockServer = (option: YockMockOption) => {
-  const server = setupServer(...getRequestHandlers(option));
-  return server;
-};
+// export const setupYockMockServer = (option: YockMockOption) => {
+//   const server = setupServer(...getRequestHandlers(option));
+//   return server;
+// };
